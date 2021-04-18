f4413aa3f340e0188ea0052965eeb2e9ccca0846
SSG\sangnd <sangnd@ssg.vn>
2021-04-18 13:24:38 +0700

    ReStudy-DinhSang: add data type and variable

--- lib/3_datatype_variables/DataTypeVariables.dart
+++ lib/3_datatype_variables/DataTypeVariables.dart
@@ -1,3 +1,19 @@
 void main() {
-  print('đây là hàm main');
+  // define a value 10
+  int age = 10;
+  var age2 = 10;
+
+  // define a value 'Henry'
+  String name = 'Henry';
+  var name2 = 'Henry';
+
+  // define a value true (Preson is alive = true or dead = false)
+  bool isAlive = true;
+  var isAlive2 = true;
+
+  print(age);
+  print(age2);
+
+  String testNull;
+  print(testNull);
 }
