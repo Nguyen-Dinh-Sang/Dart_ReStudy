2a81ae4437fecaad9700fa42142fbf063ea3a6a1
SSG\sangnd <sangnd@ssg.vn>
2021-05-09 22:26:07 +0700

    ReStudy-DinhSang: add loop control statements

---
+++ lib/7_loop_control_statements/Loop.dart
@@ -0,0 +1,17 @@
+void main() {
+  for (int i = 0; i < 4; i++) {
+    print('For loop $i');
+  }
+
+  int j = 0;
+  while (j < 4) {
+    print('While loop $j');
+    j++;
+  }
+
+  int k = 0;
+  do {
+    print('Do-While loop $k');
+    k++;
+  } while (k < 4);
+}
